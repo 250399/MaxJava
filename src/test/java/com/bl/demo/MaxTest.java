d420110487b93626c462625a821e8b3a38d228ad
250399 <kunaldeshmukh2503@gmail.com>
2020-04-27 22:21:32 -0700

    Returns Maximum of 3 integer

--- src/test/java/com/bl/demo/MaxTest.java
+++ src/test/java/com/bl/demo/MaxTest.java
@@ -1,7 +1,22 @@
 package com.bl.demo;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaxTest {
+    Max maxObject;
+
+    @Before
+    public void init(){
+        maxObject = new Max();
+    }
+
+    @Test
+    public void testPasses_ForInteger_WhenReturnsMaximum(){
+        Assert.assertEquals(10, maxObject.getMaximum(new int[]{2,5,10}));
+    }
 
 }
