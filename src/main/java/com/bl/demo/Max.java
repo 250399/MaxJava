63f0546d49d587bc28248d044fb6907d116d764c
250399 <kunaldeshmukh2503@gmail.com>
2020-04-27 22:53:56 -0700

    ADDED: Generic method which returns Maximum

--- src/main/java/com/bl/demo/Max.java
+++ src/main/java/com/bl/demo/Max.java
@@ -2,21 +2,13 @@ package com.bl.demo;
 
 public class Max {
 
-    public int getMaximum(int[] intArray){
-        int max=intArray[0];
-        for(int element=1;element<intArray.length;element++){
-            max=Math.max(max,intArray[element]);
+    public <T extends Comparable> T getMaximum(T[] array){
+        T max=array[0];
+        for(int arrayElement=1;arrayElement<array.length;arrayElement++){
+            if(array[arrayElement].compareTo(max)>0)
+                max=array[arrayElement];
         }
         return max;
-    }
-
-    public float getMaximum(float[] floatArray){
-        float max = 0;
-        for(int element=1;element<floatArray.length;element++){
-            max=Math.max(max,floatArray[element]);
-        }
-        return max;
-
     }
 
 }
