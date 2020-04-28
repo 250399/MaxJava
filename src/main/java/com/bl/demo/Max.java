58125cd34be2efd063508456c010a6a0617e7c57
250399 <kunaldeshmukh2503@gmail.com>
2020-04-28 11:12:58 -0700

    ADDED: Parameterized constructor

--- src/main/java/com/bl/demo/Max.java
+++ src/main/java/com/bl/demo/Max.java
@@ -1,14 +1,38 @@
 package com.bl.demo;
 
-public class Max {
+public class Max<T extends Comparable> {
+    T firstVariable;
+    T secondVariable;
+    T thirdVariable;
+    T[] array;
 
-    public <T extends Comparable> T getMaximum(T[] array){
-        T max=array[0];
+    Max(T firstVariable, T secondVariable ,T thirdVariable){
+        this.firstVariable=firstVariable;
+        this.secondVariable=secondVariable;
+        this.thirdVariable=thirdVariable;
+    }
+
+    Max(T[] array){
+        this.array=array;
+    }
+
+    public T getMaximum (){
+            T max = array[0];
         for(int arrayElement=1;arrayElement<array.length;arrayElement++){
             if(array[arrayElement].compareTo(max)>0)
                 max=array[arrayElement];
         }
         return max;
+    }
+
+    public T getMax(){
+        if(firstVariable.compareTo(secondVariable)>0&&firstVariable.compareTo(thirdVariable)>0)
+            return this.firstVariable;
+        else if(secondVariable.compareTo(firstVariable)>0&&secondVariable.compareTo(thirdVariable)>0)
+            return secondVariable;
+        else if(thirdVariable.compareTo(firstVariable)>0&&thirdVariable.compareTo(secondVariable)>0)
+            return thirdVariable;
+        return null;
     }
 
 }
